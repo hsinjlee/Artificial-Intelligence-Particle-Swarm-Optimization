551cceac8f477499355c5d82de207b3d16510e30
hsinjlee <hlee294@asu.edu>
2020-08-13 15:25:11 -0700

    Add Particle Swarm Optimization

---
+++ particleSwarmOptimizationApp.java
@@ -0,0 +1,16 @@
+
+
+public class particleSwarmOptimizationApp {
+
+	public static void main(String[] args) {
+		
+		ParticleSwarm algorithm = new ParticleSwarm();
+		algorithm.solve();
+		algorithm.showSolution();
+	}
+}
+/*
+olution of PSO problem!
+Best solution -> x: 2.3843904392631688E-9 - y:4.318174315439287E-9
+Value f(x,y)=1.0
+ */
